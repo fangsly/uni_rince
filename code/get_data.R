62d22bf704061766552ae15e423b2393238f5873
fangsly <viento.acariciador@gmail.com>
2018-08-25 12:50:07 +0300

    final

--- code/get_data.R
+++ code/get_data.R
@@ -10,24 +10,17 @@ library(htmltools)
 library(dplyr)
 library(xlsx)
 library(stringr)
+library(reshape2)
 proxy <- read_excel("data/proxies.xlsx")
 id_all <- scan("data\\id_uni.txt", what="", sep="\n")
 id <- as.list(id_all)
 
 error_code=TRUE
 df <-NULL
-de<-NULL
 
-df <- data.frame(ID=numeric(),
-                 Name=character(),
-                 Place=character())
-df <- setNames(df, c("ID",
-                     "Name",
-                     "Place"))
 
 j=1
-#N=length(id)
-N=1
+N=length(id)
 for (i in 1:N)
 {
   
@@ -69,14 +62,59 @@ for (i in 1:N)
   name <- main_inf %>% html_nodes("font") %>% .[1] %>% html_text()
   place <- main_inf %>% html_nodes("font") %>% .[2] %>% html_text()
   
-  de<-data.frame(c(id[i],
-                   name,
-                   place))
-  de <- setNames(de, c("ID",
-                       "Name",
-                       "Place"))
+  main_indicators <- uni_profile %>% html_nodes("table") %>% .[[23]]  %>% html_table(fill=TRUE)
+  main_indicators[-c(1:3,7,11,15),-1] ->main_indicators
+  main_indicators<-t(main_indicators)
+  colnames( main_indicators) = main_indicators[1, ]
+  main_indicators = main_indicators[-1, ]
   
-  df <- rbind(df,de)
+  dtest<-as.data.frame(cbind(id=id[i],name,place,t(main_indicators)))
+  
+  five_indicators <- uni_profile %>% html_nodes("table") %>% .[[24]]  %>% html_table(fill=TRUE)
+  five_indicators[-c(1:3,10,20,28,35),-1] ->five_indicators
+  five_indicators<-t(five_indicators)
+  colnames(five_indicators) = five_indicators[1, ]
+  five_indicators = t(five_indicators[-1, ])
+  colnames(five_indicators) <- paste(colnames(five_indicators),"5_лет",sep = "_")
+  
+  dtest2<-as.data.frame(cbind(dtest,five_indicators))
+  
+  pub_indicators <- uni_profile %>% html_nodes("table") %>% .[[25]]  %>% html_table(fill=TRUE)
+  pub_indicators %>% slice(-2:-3) %>% select(-X1)->pub_indicators
+  colnames(pub_indicators) = pub_indicators[1, ]
+  pub_indicators = pub_indicators[-1, ]
+  pub_indicators<-cbind(id[i],pub_indicators)
+  colnames(pub_indicators)[1] <- "id"
+  pub_indicators$`Область знаний`<-as.factor(pub_indicators$`Область знаний`)
+  pub_indicators<-dcast(melt(pub_indicators, id.vars=c("id","Область знаний")), id~variable+`Область знаний`)
+  colnames(pub_indicators) <- paste("пуб",colnames(pub_indicators),sep = "_")
+  
+  dtest3<-as.data.frame(cbind(dtest2,pub_indicators[,-1]))
+  
+  cit_indicators <- uni_profile %>% html_nodes("table") %>% .[[26]]  %>% html_table(fill=TRUE)
+  cit_indicators %>% slice(-2:-3) %>% select(-X1)->cit_indicators
+  colnames(cit_indicators) = cit_indicators[1, ]
+  cit_indicators = cit_indicators[-1, ]
+  cit_indicators<-cbind(id[i],cit_indicators)
+  colnames(cit_indicators)[1] <- "id"
+  cit_indicators$`Область знаний`<-as.factor(cit_indicators$`Область знаний`)
+  cit_indicators<-dcast(melt(cit_indicators, id.vars=c("id","Область знаний")), id~variable+`Область знаний`)
+  colnames(cit_indicators) <- paste("цит",colnames(cit_indicators),sep = "_")
+  
+  dtest4<-as.data.frame(cbind(dtest3,cit_indicators[,-1]))
+  
+  by_year_indicators <- uni_profile %>% html_nodes("table") %>% .[[27]]  %>% html_table(fill=TRUE)
+  by_year_indicators[-c(2,3,14,21,31,34),-1] -> by_year_indicators
+  colnames(by_year_indicators) = by_year_indicators[1, ]
+  by_year_indicators = by_year_indicators[-1, ]
+  by_year_indicators<-cbind(id[i],by_year_indicators)
+  colnames(by_year_indicators)[1] <- "id"
+  by_year_indicators$`Название показателя`<-as.factor(by_year_indicators$`Название показателя`)
+  by_year_indicators<-dcast(melt(by_year_indicators, id.vars=c("id","Название показателя")), id~`Название показателя`+variable)
+  
+  dtest5<-as.data.frame(cbind(dtest4,by_year_indicators[,-1]))
+
+  df <- rbind(df,dtest5)
   
   if ((i%%3)==0) Sys.sleep(5)
   if ((i%%20)==0) Sys.sleep(100)
@@ -95,8 +133,8 @@ for (i in 1:N)
 }
 
 date1<-str_replace_all(Sys.time(), " ", "-")
-date<-str_replace(date1, ":", "-")
-file_name<-paste_all("uni_rince_",date,".xlsx",sep="")
+date<-str_replace_all(date1, ":", "-")
+file_name<-paste("uni_rince_",date,".xlsx",sep="")
 
 write.xlsx(df,file_name)
 
